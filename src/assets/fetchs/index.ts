af0f1e518b32fba8a6826421805081e81abafb01
Gianni Bussoletti <BussolettiGianni@gmail.com>
2026-09-08 20:12:21 +0200

    return localhost not in deploy

--- src/assets/fetchs/index.ts
+++ src/assets/fetchs/index.ts
@@ -11,9 +11,7 @@ import type {
 
 export const fetchCinemas = async (): Promise<CinemaFetchType[]> => {
   try {
-    const res = await fetch(
-      "https://be-adadstra-capstone-epicode-production.up.railway.app/public/cinemas",
-    )
+    const res = await fetch("http://localhost:5555/public/cinemas")
 
     if (!res.ok) {
       console.log(res)
@@ -28,18 +26,17 @@ export const fetchCinemas = async (): Promise<CinemaFetchType[]> => {
   }
 }
 
-export const fetchScreenTimes = async (cinemaId: string): Promise<MovieGroup[]> => {
+export const fetchScreenTimes = async (
+  cinemaId: string,
+): Promise<MovieGroup[]> => {
   try {
-    const res = await fetch(
-      "https://be-adadstra-capstone-epicode-production.up.railway.app/public/screening-times",
-      {
-        method: "POST",
-        headers: {
-          "Content-Type": "application/json",
-        },
-        body: JSON.stringify({ cinemaId }),
+    const res = await fetch("http://localhost:5555/public/screening-times", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
       },
-    )
+      body: JSON.stringify({ cinemaId }),
+    })
 
     if (!res.ok) {
       console.log(res)
@@ -52,18 +49,18 @@ export const fetchScreenTimes = async (cinemaId: string): Promise<MovieGroup[]> 
     throw err
   }
 }
-export const fetchSeats = async (cinemaId: string, screenId: string): Promise<SeatGroup[]> => {
+export const fetchSeats = async (
+  cinemaId: string,
+  screenId: string,
+): Promise<SeatGroup[]> => {
   try {
-    const res = await fetch(
-      "https://be-adadstra-capstone-epicode-production.up.railway.app/public/seats",
-      {
-        method: "POST",
-        headers: {
-          "Content-Type": "application/json",
-        },
-        body: JSON.stringify({ cinemaId, screenId }),
+    const res = await fetch("http://localhost:5555/public/seats", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
       },
-    )
+      body: JSON.stringify({ cinemaId, screenId }),
+    })
 
     if (!res.ok) {
       console.log(res)
@@ -77,18 +74,17 @@ export const fetchSeats = async (cinemaId: string, screenId: string): Promise<Se
   }
 }
 
-export const fetchBookedSeats = async (screeningTimeId: string): Promise<SeatGroup[]> => {
+export const fetchBookedSeats = async (
+  screeningTimeId: string,
+): Promise<SeatGroup[]> => {
   try {
-    const res = await fetch(
-      "https://be-adadstra-capstone-epicode-production.up.railway.app/public/tickets",
-      {
-        method: "POST",
-        headers: {
-          "Content-Type": "application/json",
-        },
-        body: JSON.stringify({ screeningTimeId }),
+    const res = await fetch("http://localhost:5555/public/tickets", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
       },
-    )
+      body: JSON.stringify({ screeningTimeId }),
+    })
 
     if (!res.ok) {
       console.log(res)
@@ -109,9 +105,8 @@ export const fetchBooking = async (
   guestEmail: string,
   coupon: string,
 ): Promise<BookingType> => {
-  const publicBooking =
-    "https://be-adadstra-capstone-epicode-production.up.railway.app/public/bookings"
-  const noPublic = "https://be-adadstra-capstone-epicode-production.up.railway.app/bookings"
+  const publicBooking = "http://localhost:5555/public/bookings"
+  const noPublic = "http://localhost:5555/bookings"
   try {
     const isLogged = localStorage.getItem("accessToken")
     const res = await fetch(isLogged ? noPublic : publicBooking, {
@@ -149,22 +144,19 @@ export const fetchRegistration = async (
   password: string,
 ): Promise<BookingType> => {
   try {
-    const res = await fetch(
-      "https://be-adadstra-capstone-epicode-production.up.railway.app/auth/registration",
-      {
-        method: "POST",
-        headers: {
-          "Content-Type": "application/json",
-        },
-        body: JSON.stringify({
-          name,
-          surname,
-          birthDate,
-          email,
-          password,
-        }),
+    const res = await fetch("http://localhost:5555/auth/registration", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
       },
-    )
+      body: JSON.stringify({
+        name,
+        surname,
+        birthDate,
+        email,
+        password,
+      }),
+    })
 
     if (!res.ok) {
       console.log(res)
@@ -178,21 +170,21 @@ export const fetchRegistration = async (
   }
 }
 
-export const fetchLogin = async (email: string, password: string): Promise<ProfileType> => {
+export const fetchLogin = async (
+  email: string,
+  password: string,
+): Promise<ProfileType> => {
   try {
-    const res = await fetch(
-      "https://be-adadstra-capstone-epicode-production.up.railway.app/auth/login",
-      {
-        method: "POST",
-        headers: {
-          "Content-Type": "application/json",
-        },
-        body: JSON.stringify({
-          email,
-          password,
-        }),
+    const res = await fetch("http://localhost:5555/auth/login", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
       },
-    )
+      body: JSON.stringify({
+        email,
+        password,
+      }),
+    })
 
     if (!res.ok) {
       console.log(res)
@@ -208,9 +200,7 @@ export const fetchLogin = async (email: string, password: string): Promise<Profi
 
 export const fetchMovies = async (): Promise<MovieDetails[]> => {
   try {
-    const res = await fetch(
-      "https://be-adadstra-capstone-epicode-production.up.railway.app/public/movies",
-    )
+    const res = await fetch("http://localhost:5555/public/movies")
 
     if (!res.ok) {
       console.log(res)
@@ -224,11 +214,11 @@ export const fetchMovies = async (): Promise<MovieDetails[]> => {
   }
 }
 
-export const fetchMovieDetails = async (movieId: string): Promise<MovieDetails> => {
+export const fetchMovieDetails = async (
+  movieId: string,
+): Promise<MovieDetails> => {
   try {
-    const res = await fetch(
-      "https://be-adadstra-capstone-epicode-production.up.railway.app/public/movies/" + movieId,
-    )
+    const res = await fetch("http://localhost:5555/public/movies/" + movieId)
 
     if (!res.ok) {
       console.log(res)
@@ -244,18 +234,15 @@ export const fetchMovieDetails = async (movieId: string): Promise<MovieDetails> 
 
 export const verifyAccessToken = async (token: string): Promise<void> => {
   try {
-    const res = await fetch(
-      "https://be-adadstra-capstone-epicode-production.up.railway.app/auth/token",
-      {
-        method: "POST",
-        headers: {
-          "Content-Type": "application/json",
-        },
-        body: JSON.stringify({
-          token,
-        }),
+    const res = await fetch("http://localhost:5555/auth/token", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
       },
-    )
+      body: JSON.stringify({
+        token,
+      }),
+    })
 
     if (!res.ok) {
       console.log(res)
@@ -271,14 +258,11 @@ export const verifyAccessToken = async (token: string): Promise<void> => {
 export const fetchUserMovies = async (): Promise<UserMovies[]> => {
   try {
     const isLogged = localStorage.getItem("accessToken")
-    const res = await fetch(
-      "https://be-adadstra-capstone-epicode-production.up.railway.app/tickets/user-movies",
-      {
-        headers: {
-          Authorization: "Bearer " + isLogged,
-        },
+    const res = await fetch("http://localhost:5555/tickets/user-movies", {
+      headers: {
+        Authorization: "Bearer " + isLogged,
       },
-    )
+    })
 
     if (!res.ok) {
       console.log(res)
@@ -298,20 +282,17 @@ export const fetchUpdatePsw = async (
 ): Promise<UpdateResponse> => {
   try {
     const isLogged = localStorage.getItem("accessToken")
-    const res = await fetch(
-      "https://be-adadstra-capstone-epicode-production.up.railway.app/user/profile/password",
-      {
-        method: "PATCH",
-        headers: {
-          Authorization: "Bearer " + isLogged,
-          "Content-Type": "application/json",
-        },
-        body: JSON.stringify({
-          oldPassword,
-          newPassword,
-        }),
+    const res = await fetch("http://localhost:5555/user/profile/password", {
+      method: "PATCH",
+      headers: {
+        Authorization: "Bearer " + isLogged,
+        "Content-Type": "application/json",
       },
-    )
+      body: JSON.stringify({
+        oldPassword,
+        newPassword,
+      }),
+    })
     console.log(res)
     if (!res.ok) {
       console.log(res)
@@ -325,22 +306,21 @@ export const fetchUpdatePsw = async (
   }
 }
 
-export const fetchUpdateMail = async (newEmail: string): Promise<UpdateResponse> => {
+export const fetchUpdateMail = async (
+  newEmail: string,
+): Promise<UpdateResponse> => {
   try {
     const isLogged = localStorage.getItem("accessToken")
-    const res = await fetch(
-      "https://be-adadstra-capstone-epicode-production.up.railway.app/user/profile/new-email",
-      {
-        method: "PATCH",
-        headers: {
-          Authorization: "Bearer " + isLogged,
-          "Content-Type": "application/json",
-        },
-        body: JSON.stringify({
-          newEmail,
-        }),
+    const res = await fetch("http://localhost:5555/user/profile/new-email", {
+      method: "PATCH",
+      headers: {
+        Authorization: "Bearer " + isLogged,
+        "Content-Type": "application/json",
       },
-    )
+      body: JSON.stringify({
+        newEmail,
+      }),
+    })
     console.log(res)
     if (!res.ok) {
       console.log(res)
@@ -354,23 +334,22 @@ export const fetchUpdateMail = async (newEmail: string): Promise<UpdateResponse>
   }
 }
 
-export const fetchUpdateProPic = async (image: FileList): Promise<{ imageLink: string }> => {
+export const fetchUpdateProPic = async (
+  image: FileList,
+): Promise<{ imageLink: string }> => {
   try {
     const formData = new FormData()
     formData.append("avatar_pic", image[0])
 
     const isLogged = localStorage.getItem("accessToken")
 
-    const res = await fetch(
-      "https://be-adadstra-capstone-epicode-production.up.railway.app/user/profile/avatar",
-      {
-        method: "PATCH",
-        headers: {
-          Authorization: "Bearer " + isLogged,
-        },
-        body: formData,
+    const res = await fetch("http://localhost:5555/user/profile/avatar", {
+      method: "PATCH",
+      headers: {
+        Authorization: "Bearer " + isLogged,
       },
-    )
+      body: formData,
+    })
     console.log(res)
     if (!res.ok) {
       console.log(res)
